57babdb97a5c35897143bafc07060d3bcf9a8833
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2014-10-07 12:29:04 +0400

    Добавлена папка с проектом
    Добавлена папка, содержащая главный класс игры jArk.

---
+++ jArk/src/jark/JArk.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jark;
+
+/**
+ *
+ * @author пользователь
+ */
+public class JArk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
