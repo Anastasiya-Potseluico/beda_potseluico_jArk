e0bc36ea1312f6757a9067812cc805f5fdf99b78
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2014-10-07 23:46:10 +0400

    Добавление пакета физического представления
    Добавлены классы физического представления всех элементов игры

---
+++ jArk/src/PhysicalObjects/PhysicalGameField.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PhysicalObjects;
+
+/**
+ * Класс физического представления игрового поля
+ * @author пользователь
+ */
+public class PhysicalGameField {
+    
+    /**
+    * Массив элементов, содержащихся на поле
+    */
+    private PhysicalElementField _field[];
+    
+}
