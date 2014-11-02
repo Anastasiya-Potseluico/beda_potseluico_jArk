c8374997f9e647cdb0d8a636c1c78a90cef9b914
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2014-11-02 22:17:16 +0400

    Добавлен класс тестов для элемента поля

---
+++ jArk/test/jark/model/ElementFieldTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jark.model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author пользователь
+ */
+public class ElementFieldTest {
+    
+    public ElementFieldTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of reactOnCollision method, of class ElementField.
+     */
+    @Test
+    public void testReactOnCollision() {
+        System.out.println("reactOnCollision");
+        ElementField element = null;
+        ElementField instance = new ElementFieldImpl();
+        instance.reactOnCollision(element);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    public class ElementFieldImpl extends ElementField {
+    }
+    
+}
