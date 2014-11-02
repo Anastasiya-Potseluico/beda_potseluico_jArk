21d292bc027b94ecb733a52950784f4fce04f622
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2014-11-02 22:16:54 +0400

    Добавлен класс тестов для мяча

---
+++ jArk/test/jark/model/BallTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jark.model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author пользователь
+ */
+public class BallTest {
+    
+    public BallTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of rebound method, of class Ball.
+     */
+    @Test
+    public void testRebound() {
+        System.out.println("rebound");
+        ElementField element = null;
+        Ball instance = new Ball();
+        boolean expResult = false;
+        boolean result = instance.rebound(element);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
