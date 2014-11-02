f539a628a547abe5b05a039cb781a002937243b8
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2014-11-02 22:17:52 +0400

    Добавлен класс тестов для роя

---
+++ jArk/test/jark/model/SwarmTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jark.model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author пользователь
+ */
+public class SwarmTest {
+    
+    public SwarmTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of insertBall method, of class Swarm.
+     */
+    @Test
+    public void testInsertBall() {
+        System.out.println("insertBall");
+        Ball ball = null;
+        Swarm instance = new Swarm();
+        boolean expResult = false;
+        boolean result = instance.insertBall(ball);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
