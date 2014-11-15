478bcda28100326f925dbe62e4385d7a879436cd
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2014-11-15 15:58:59 +0400

    Начат интерфейс менеджера коллизий

--- jArk/src/jark/collisionManagers/CollisionManager.java
+++ jArk/src/jark/collisionManagers/CollisionManager.java
@@ -5,10 +5,33 @@
  */
 package jark.collisionManagers;
 
+import com.golden.gamedev.object.collision.AdvanceCollisionGroup;
+import jArk.physicalObjects.ElementFieldView;
+
 /**
  * Класс обработчика коллизий
  * @author пользователь
  */
 public class CollisionManager {
+    
+    /**Стандартный обработчик коллизий */
+    private AdvanceCollisionGroup _collision;
+    
+    /**
+     * Метод для обработки коллизии
+     */
+    public void manageCollision(){
+        
+    }
+    
+    /**
+     * Метод для добавления групп коллизий (чтобы менеджер не обращался к полю)
+     * @param group1 первая группа представлений элементов
+     * @param group2 вторая группа представлений элементов
+     */
+    public void setCollisionGroup(ElementFieldView group1[], ElementFieldView group2[]){
+        
+    }
+    
     
 }
