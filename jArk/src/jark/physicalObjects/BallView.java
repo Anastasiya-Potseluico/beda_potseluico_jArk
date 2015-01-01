3f6dbbd70fa3c5a99fe3779bc891f758e6bb2aee
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-01-01 17:58:08 +0400

    Изменен класс физического представления мяча

--- jArk/src/jark/physicalObjects/BallView.java
+++ jArk/src/jark/physicalObjects/BallView.java
@@ -6,20 +6,38 @@
 
 package jArk.physicalObjects;
 
+import com.golden.gamedev.object.Sprite;
 import jark.model.Ball;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /**
  * Класс физического представления мяча
  * @author пользователь
  */
 public class BallView extends ElementFieldView{
-   /** Логическое представление мяча */
-    private Ball _ball;
-    
     /**
     * Конструктор
+     * @param ball
     */ 
-    public BallView() {
+    public BallView(Ball ball, int x, int y) {
+        this._elementField = ball;
+        BufferedImage img = null;
+        try 
+        {
+            img = ImageIO.read(new File("C:\\Users\\пользователь\\Documents\\GitHub\\beda_potseluico_jArk\\jArk\\src\\jark\\ball.png")); // eventually C:\\ImageTest\\pic2.jpg
+            this._elementSprite = new Sprite(img, x, y);
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
     }
     
     /**
@@ -27,7 +45,7 @@ public class BallView extends ElementFieldView{
      * @return мяч
      */
     public Ball ball(){
-        return _ball;
+        return (Ball)_elementField;
     }
     
     /**
@@ -35,6 +53,6 @@ public class BallView extends ElementFieldView{
      * @param ball мяч
      */
     public void setBall(Ball ball) {
-        _ball = ball;
+        _elementField = ball;
     }
 }
