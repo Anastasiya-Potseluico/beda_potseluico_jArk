686fa36f4bad4af60f2bfc9370a9c69e7a425fc4
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-01-03 01:43:32 +0400

    Размер игры подогнан под более адекватный

--- jArk/src/jark/JArk.java
+++ jArk/src/jark/JArk.java
@@ -20,13 +20,9 @@ public class JArk {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        File file = new File("test.txt");
-        String path = file.getAbsolutePath();
-        // TODO code application logic here
         GameLoader game = new GameLoader();
-        game.setup(new JarkGame(), new Dimension(640,640), false);
+        game.setup(new JarkGame(), new Dimension(640,550), false);
         game.start();
-        
     }
     
 }
