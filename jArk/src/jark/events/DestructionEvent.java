c625307b3a6c63e9b7e7efa2d00ab3deae86d8f1
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-01-08 14:38:35 +0400

    Добавлены классы: событие и слушатель

---
+++ jArk/src/jark/events/DestructionEvent.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jark.events;
+
+import java.util.EventObject;
+
+/**
+ *
+ * @author пользователь
+ */
+public class DestructionEvent extends EventObject{
+    public DestructionEvent(Object o) {
+        super(o);
+    }
+}
