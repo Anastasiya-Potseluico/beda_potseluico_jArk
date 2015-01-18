19bc1f959a10a77865c6fd4cf8236ed41bc3887f
DaryaBeda <beda1304@gmail.com>
2015-01-18 15:39:25 +0400

    Документирован класс игрока

--- jArk/src/jark/Player.java
+++ jArk/src/jark/Player.java
@@ -7,38 +7,58 @@ package jark;
 
 /**
  * Класс игрока
+ *
  * @author Дарья
  */
 public class Player {
-    /** Количество жизней */
-    private int _number_of_lives;
-    
-    private int _scores;
+
     /**
-     * Устанавливает количество жизней
-     * @param number количество жизней
+     * Количество жизней
+     */
+    private int _number_of_lives;
+    /**
+     * Количество очков
+     */
+    private int _scores;
+
+    /**
+     * Конструктор
      */
     public Player() {
         _number_of_lives = 3;
         _scores = 0;
     }
+
     /**
      * Возвращает количество жизней
+     *
      * @return количество жизней
      */
-    public int numberOfLives()
-    {
+    public int numberOfLives() {
         return _number_of_lives;
     }
-    
+
+    /**
+     * Уменьшает количество жизней
+     */
     public void reduceNumberOfLives() {
         _number_of_lives--;
     }
-    
+
+    /**
+     * Возвращает количество очков
+     *
+     * @return количество очков
+     */
     public int scores() {
         return _scores;
     }
-    
+
+    /**
+     * Увеличивает количество очков на указанную сумму
+     *
+     * @param toScores сумма очков
+     */
     public void sumScore(int toScores) {
         _scores += toScores;
     }
