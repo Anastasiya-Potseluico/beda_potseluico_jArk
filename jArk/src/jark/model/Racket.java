3536965c8be025a08931deb96630a962367398a2
DaryaBeda <beda1304@gmail.com>
2015-01-18 15:25:06 +0400

    Документированы классы ракетки

--- jArk/src/jark/model/Racket.java
+++ jArk/src/jark/model/Racket.java
@@ -5,38 +5,54 @@
  */
 package jark.model;
 
-import com.golden.gamedev.object.Sprite;
 import jark.collisionManagers.CollisionMan;
 import jark.specifications.Buffer;
 
 /**
  * Класс ракетки
+ *
  * @author Дарья
  */
-public class Racket extends ElementField{
+public class Racket extends ElementField {
 
-    boolean _hasBall;
     /**
      * Конструктор ракетки
+     *
+     * @param x координата по оси Х
+     * @param y координата по оси У
      */
     public Racket(int x, int y) {
-        super(MASS.INF_MASS,x,y);
+        super(MASS.INF_MASS, x, y);
     }
 
+    /**
+     * Установить скорость
+     *
+     * @param vX Горизонтальная скорость
+     * @param vY Вертикальная скорость
+     * @return Успех
+     */
     @Override
     public boolean setSpeed(double vX, double vY) {
-        if(Buffer.findSprite(this).getX() <= 5)
+        if (Buffer.findSprite(this).getX() <= 5) {
             return super.setSpeed(Math.abs(vX), vY);
-        else if (Buffer.findSprite(this).getX() >= 497)
-            return super.setSpeed(Math.abs(vX)*-1, vY);
-        else
-            return super.setSpeed(vX, vY);        
+        } else if (Buffer.findSprite(this).getX() >= 497) {
+            return super.setSpeed(Math.abs(vX) * -1, vY);
+        } else {
+            return super.setSpeed(vX, vY);
+        }
     }
 
+    /**
+     * Функция реагирования на столкновение
+     *
+     * @param element Элемент,с которым произошло столкновение
+     * @param type Тип столкновения
+     */
     @Override
     public void reactOnCollision(ElementField element, CollisionMan.TYPE type) {
-        if(element instanceof BoundaryField) {
-            setSpeed(Buffer.findSprite(this).getHorizontalSpeed()*-1, 0); 
+        if (element instanceof BoundaryField) {
+            setSpeed(Buffer.findSprite(this).getHorizontalSpeed() * -1, 0);
         }
     }
 }
--- jArk/src/jark/view/RacketView.java
+++ jArk/src/jark/view/RacketView.java
@@ -3,11 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package jark.view;
 
 import com.golden.gamedev.object.Sprite;
-import jark.model.Racket;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -15,21 +13,24 @@ import javax.imageio.ImageIO;
 
 /**
  * Класс физического представления ракетки
+ *
  * @author пользователь
  */
-public class RacketView extends ElementFieldView{
-    
+public class RacketView extends ElementFieldView {
+
+    /**
+     * Конструктор
+     *
+     * @param x координата по оси Х
+     * @param y координата по оси У
+     */
     public RacketView(int x, int y) {
         BufferedImage img = null;
-        try 
-        {
+        try {
             img = ImageIO.read(new File("src\\jark\\racket.png"));
-            this._elementSprite = new Sprite(img, x, y);
-            this._elementSprite.setID(5);
+            _elementSprite = new Sprite(img, x, y);
             _elementSprite.setSpeed(0, 0);
-        } 
-        catch (IOException e) 
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
