b1590d0cc239ba3e4a4735f88b1bda5f6f4b44ec
DaryaBeda <beda1304@gmail.com>
2015-01-18 19:26:44 +0400

    Добавлена часть тестов для мяча

--- jArk/test/jark/model/BallTest.java
+++ jArk/test/jark/model/BallTest.java
@@ -5,6 +5,13 @@
  */
 package jark.model;
 
+import com.golden.gamedev.object.Sprite;
+import jark.collisionManagers.CollisionMan;
+import jark.specifications.Buffer;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,39 +24,214 @@ import static org.junit.Assert.*;
  * @author пользователь
  */
 public class BallTest {
-    
+
     public BallTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
+
     }
-    
+
     @After
     public void tearDown() {
     }
 
     /**
-     * Test of rebound method, of class Ball.
+     * мяч летит под прямым углом, ракетка не подвижна
      */
     @Test
-    public void testRebound() {
-       /* System.out.println("rebound");
-        ElementField element = null;
-        Ball instance = new Ball(5);
-        boolean expResult = false;
-        boolean result = instance.rebound(element);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");*/
+    public void testRightAngleRacketNotMove() throws IOException {
+        System.out.println("мяч летит под прямым углом, ракетка не подвижна");
+        ElementField ball = new Ball(295, 200);
+        ElementField racket = new Racket(250, 500);
+        BufferedImage img = ImageIO.read(new File("src\\jark\\ball.png"));
+        Sprite ballSprite = new Sprite(img, 295, 200);
+        img = ImageIO.read(new File("src\\jark\\racket.png"));
+        Sprite racketSprite = new Sprite(img, 250, 500);
+        Buffer.addElement(ball, ballSprite);
+        Buffer.addElement(racket, racketSprite);
+        ball.setSpeed(0, 0.3);
+        racket.setSpeed(0, 0);
+        ball.reactOnCollision(racket, CollisionMan.TYPE.DEFAULT);
+        assertEquals(-0.3, Buffer.findSprite(ball).getVerticalSpeed(), 0.1);
+        assertEquals(0.0, Buffer.findSprite(ball).getHorizontalSpeed(), 0.1);
     }
-    
+
+    /**
+     * мяч летит слева, ракетка не подвижна
+     */
+    @Test
+    public void testLeftRacketNotMove() throws IOException {
+        System.out.println("мяч летит слева, ракетка не подвижна");
+        ElementField ball = new Ball(250, 430);
+        ElementField racket = new Racket(250, 500);
+        BufferedImage img = ImageIO.read(new File("src\\jark\\ball.png"));
+        Sprite ballSprite = new Sprite(img, 250, 430);
+        img = ImageIO.read(new File("src\\jark\\racket.png"));
+        Sprite racketSprite = new Sprite(img, 250, 500);
+        Buffer.addElement(ball, ballSprite);
+        Buffer.addElement(racket, racketSprite);
+        ball.setSpeed(0.3, 0.3);
+        racket.setSpeed(0, 0);
+        ball.reactOnCollision(racket, CollisionMan.TYPE.DEFAULT);
+        assertEquals(-0.3, Buffer.findSprite(ball).getVerticalSpeed(), 0.1);
+        assertEquals(0.3, Buffer.findSprite(ball).getHorizontalSpeed(), 0.1);
+    }
+
+    /**
+     * мяч летит справа, ракетка не подвижна
+     */
+    @Test
+    public void testRightRacketNotMove() throws IOException {
+        System.out.println("мяч летит справа, ракетка не подвижна");
+        ElementField ball = new Ball(340, 430);
+        ElementField racket = new Racket(250, 500);
+        BufferedImage img = ImageIO.read(new File("src\\jark\\ball.png"));
+        Sprite ballSprite = new Sprite(img, 340, 430);
+        img = ImageIO.read(new File("src\\jark\\racket.png"));
+        Sprite racketSprite = new Sprite(img, 250, 500);
+        Buffer.addElement(ball, ballSprite);
+        Buffer.addElement(racket, racketSprite);
+        ball.setSpeed(-0.3, 0.3);
+        racket.setSpeed(0, 0);
+        ball.reactOnCollision(racket, CollisionMan.TYPE.DEFAULT);
+        assertEquals(-0.3, Buffer.findSprite(ball).getVerticalSpeed(), 0.1);
+        assertEquals(-0.3, Buffer.findSprite(ball).getHorizontalSpeed(), 0.1);
+    }
+
+    /**
+     * мяч летит под прямым углом, ракетка движется вправо
+     */
+    @Test
+    public void testRightAngleRacketMoveRight() throws IOException {
+        System.out.println("мяч летит под прямым углом, ракетка движется вправо");
+        ElementField ball = new Ball(350, 420);
+        ElementField racket = new Racket(250, 500);
+        BufferedImage img = ImageIO.read(new File("src\\jark\\ball.png"));
+        Sprite ballSprite = new Sprite(img, 350, 420);
+        img = ImageIO.read(new File("src\\jark\\racket.png"));
+        Sprite racketSprite = new Sprite(img, 250, 500);
+        Buffer.addElement(ball, ballSprite);
+        Buffer.addElement(racket, racketSprite);
+        ball.setSpeed(0, 0.3);
+        racket.setSpeed(0.1, 0);
+        ball.reactOnCollision(racket, CollisionMan.TYPE.DEFAULT);
+        assertEquals(-0.3, Buffer.findSprite(ball).getVerticalSpeed(), 0.1);
+        assertEquals(0.2, Buffer.findSprite(ball).getHorizontalSpeed(), 0.1);
+    }
+
+    /**
+     * мяч летит слева, ракетка движется вправо
+     */
+    @Test
+    public void testLeftRacketMoveRight() throws IOException {
+        System.out.println("мяч летит слева, ракетка движется вправо");
+        ElementField ball = new Ball(250, 430);
+        ElementField racket = new Racket(250, 500);
+        BufferedImage img = ImageIO.read(new File("src\\jark\\ball.png"));
+        Sprite ballSprite = new Sprite(img, 250, 430);
+        img = ImageIO.read(new File("src\\jark\\racket.png"));
+        Sprite racketSprite = new Sprite(img, 250, 500);
+        Buffer.addElement(ball, ballSprite);
+        Buffer.addElement(racket, racketSprite);
+        ball.setSpeed(0.3, 0.3);
+        racket.setSpeed(0.1, 0);
+        ball.reactOnCollision(racket, CollisionMan.TYPE.DEFAULT);
+        assertEquals(-0.3, Buffer.findSprite(ball).getVerticalSpeed(), 0.1);
+        assertEquals(0.3, Buffer.findSprite(ball).getHorizontalSpeed(), 0.1);
+    }
+
+    /**
+     * мяч летит справа, ракетка движется вправо
+     */
+    @Test
+    public void testRightRacketMoveRight() throws IOException {
+        System.out.println("мяч летит справа, ракетка движется вправо");
+        ElementField ball = new Ball(340, 430);
+        ElementField racket = new Racket(250, 500);
+        BufferedImage img = ImageIO.read(new File("src\\jark\\ball.png"));
+        Sprite ballSprite = new Sprite(img, 340, 430);
+        img = ImageIO.read(new File("src\\jark\\racket.png"));
+        Sprite racketSprite = new Sprite(img, 250, 500);
+        Buffer.addElement(ball, ballSprite);
+        Buffer.addElement(racket, racketSprite);
+        ball.setSpeed(-0.3, 0.3);
+        racket.setSpeed(0.1, 0);
+        ball.reactOnCollision(racket, CollisionMan.TYPE.DEFAULT);
+        assertEquals(-0.3, Buffer.findSprite(ball).getVerticalSpeed(), 0.1);
+        assertEquals(0.3, Buffer.findSprite(ball).getHorizontalSpeed(), 0.1);
+    }
+
+    /**
+     * мяч летит под прямым углом, ракетка движется влево
+     */
+    @Test
+    public void testRightAngleRacketMoveLeft() throws IOException {
+        System.out.println("мяч летит под прямым углом, ракетка движется влево");
+        ElementField ball = new Ball(300, 420);
+        ElementField racket = new Racket(250, 500);
+        BufferedImage img = ImageIO.read(new File("src\\jark\\ball.png"));
+        Sprite ballSprite = new Sprite(img, 300, 420);
+        img = ImageIO.read(new File("src\\jark\\racket.png"));
+        Sprite racketSprite = new Sprite(img, 250, 500);
+        Buffer.addElement(ball, ballSprite);
+        Buffer.addElement(racket, racketSprite);
+        ball.setSpeed(0, 0.3);
+        racket.setSpeed(-0.1, 0);
+        ball.reactOnCollision(racket, CollisionMan.TYPE.DEFAULT);
+        assertEquals(-0.3, Buffer.findSprite(ball).getVerticalSpeed(), 0.1);
+        assertEquals(-0.2, Buffer.findSprite(ball).getHorizontalSpeed(), 0.1);
+    }
+
+    /**
+     * мяч летит слева, ракетка движется влево
+     */
+    @Test
+    public void testLeftRacketMoveLeft() throws IOException {
+        System.out.println("мяч летит слева, ракетка движется влево");
+        ElementField ball = new Ball(250, 430);
+        ElementField racket = new Racket(250, 500);
+        BufferedImage img = ImageIO.read(new File("src\\jark\\ball.png"));
+        Sprite ballSprite = new Sprite(img, 250, 430);
+        img = ImageIO.read(new File("src\\jark\\racket.png"));
+        Sprite racketSprite = new Sprite(img, 250, 500);
+        Buffer.addElement(ball, ballSprite);
+        Buffer.addElement(racket, racketSprite);
+        ball.setSpeed(0.3, 0.3);
+        racket.setSpeed(-0.1, 0);
+        ball.reactOnCollision(racket, CollisionMan.TYPE.DEFAULT);
+        assertEquals(-0.3, Buffer.findSprite(ball).getVerticalSpeed(), 0.1);
+        assertEquals(-0.3, Buffer.findSprite(ball).getHorizontalSpeed(), 0.1);
+    }
+
+    /**
+     * мяч летит справа, ракетка движется влево
+     */
+    @Test
+    public void testRightRacketMoveLeft() throws IOException {
+        System.out.println("мяч летит справа, ракетка движется влево");
+        ElementField ball = new Ball(340, 430);
+        ElementField racket = new Racket(250, 500);
+        BufferedImage img = ImageIO.read(new File("src\\jark\\ball.png"));
+        Sprite ballSprite = new Sprite(img, 340, 430);
+        img = ImageIO.read(new File("src\\jark\\racket.png"));
+        Sprite racketSprite = new Sprite(img, 250, 500);
+        Buffer.addElement(ball, ballSprite);
+        Buffer.addElement(racket, racketSprite);
+        ball.setSpeed(-0.3, 0.3);
+        racket.setSpeed(-0.1, 0);
+        ball.reactOnCollision(racket, CollisionMan.TYPE.DEFAULT);
+        assertEquals(-0.3, Buffer.findSprite(ball).getVerticalSpeed(), 0.1);
+        assertEquals(-0.3, Buffer.findSprite(ball).getHorizontalSpeed(), 0.1);
+    }
+
 }
