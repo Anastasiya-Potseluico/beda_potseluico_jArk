9fa31350987b674d998a6f1a7c55ca8441bd978d
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-01-18 15:50:45 +0400

    Задокументирован класс неразбиваемого кирпича

--- jArk/src/jark/view/IndestructibleBrickView.java
+++ jArk/src/jark/view/IndestructibleBrickView.java
@@ -7,7 +7,6 @@
 package jark.view;
 
 import com.golden.gamedev.object.Sprite;
-import jark.model.IndestructibleBrick;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -18,14 +17,17 @@ import javax.imageio.ImageIO;
  * @author пользователь
  */
 public class IndestructibleBrickView extends ElementFieldView{
-    
+    /**
+     * Конструктор физического представления разрушаемого кирпича
+     * @param x координата х кирпичана поле
+     * @param y координата у кирпичана поле
+     */
     public IndestructibleBrickView (int x, int y) {
         BufferedImage img = null;
         try 
         {
             img = ImageIO.read(new File("src\\jark\\ibrick.png"));
             this._elementSprite = new Sprite(img,x,y);
-            this._elementSprite.setID(4);
         } 
         catch (IOException e) 
         {
